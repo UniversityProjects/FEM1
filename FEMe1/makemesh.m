7dcfcd023fed036fb488f413918f531babf7c7bc
Francesco Saporito <sapo93@gmail.com>
2016-04-22 15:52:58 +0200

    Update makemesh.m

--- FEMe1/makemesh.m
+++ FEMe1/makemesh.m
@@ -1,3 +1,12 @@
 % Generates the mesh from the .poly file
+% Uses the software triangle: https://www.cs.cmu.edu/~quake/triangle.html
 
-system(['triangle -pqaI ' omega '.poly']);
+% Triangle Options
+% -p Triangulates a .poly file
+% -q Quality mesh generation with no angles smaller than 20 degrees. 
+% -a Imposes a maximum triangle area constraint (Read from the .poly file)
+% -I Suppresses mesh iteration numbers
+% -Q No output except for errors
+
+% Generate the mesh specified in the .poly file
+system(['triangle -pqaIQ ' omega '.poly']);
